6c6f592fb4bee34b9086c3be38c23e3b0afc9585
matheus-eel2011 <matheus.eel2011@gmail.com>
2026-01-08 22:43:34 -0300

    Update tournaments.js

--- api/tournaments.js
+++ api/tournaments.js
@@ -223,18 +223,19 @@ const dailyHyperTemplate = [
   { time: "23:45", name: "Daily Hyper $2", buyin: 2, guaranteed: 600 },
 ];
 
-const ggmasterTemplate = [
-  { time: "00:45", name: "Daily master $2", buyin: 2, guaranteed: 1000 },
-  { time: "00:45", name: "Daily master $50", buyin: 50, guaranteed: 2000 },
-  { time: "01:45", name: "Daily master $20", buyin: 20, guaranteed: 2500 },
-  { time: "02:45", name: "Daily master $10", buyin: 10, guaranteed: 1500 },
-  { time: "03:45", name: "Daily master $100", buyin: 100, guaranteed: 2000 },
-];
+
 const dailyTurboTemplate = [
   { time: "00:15", name: "Daily Turbo $10", buyin: 10, guaranteed: 600 },
   { time: "01:15", name: "Daily Turbo $20", buyin: 20, guaranteed: 500 },
   { time: "02:15", name: "Daily Turbo $40", buyin: 40, guaranteed: 500 },
   { time: "03:15", name: "Daily Turbo $5", buyin: 5, guaranteed: 400 },
+];
+
+const ggmasterTemplate = [
+  { time: "00:15", name: "Daily master $10", buyin: 10, guaranteed: 600 },
+  { time: "01:15", name: "Daily master $20", buyin: 20, guaranteed: 500 },
+  { time: "02:15", name: "Daily master $40", buyin: 40, guaranteed: 500 },
+  { time: "03:15", name: "Daily master $5", buyin: 5, guaranteed: 400 },
 ];
 
 const sundaySpecials = [
@@ -281,6 +282,22 @@ dates.forEach(date => {
     });
   });
 
+  dailyTurboTemplate.forEach(template => {
+    tournaments.push({
+      id: id++,
+      date: dateStr,
+      time: template.time,
+      site: "GGPoker",
+      name: template.name,
+      type: "TURBO",
+      buyin: template.buyin,
+      guaranteed: template.guaranteed,
+      priority: "very-high",
+      status: "Aberto"
+    });
+  });
+});
+
 // Adicionar GGPoker GGMaster para cada dia
 dates.forEach(date => {
   const dateStr = formatDateString(date);
@@ -299,23 +316,7 @@ dates.forEach(date => {
       status: "Aberto"
     });
   });
-
-  dailyTurboTemplate.forEach(template => {
-    tournaments.push({
-      id: id++,
-      date: dateStr,
-      time: template.time,
-      site: "GGPoker",
-      name: template.name,
-      type: "TURBO",
-      buyin: template.buyin,
-      guaranteed: template.guaranteed,
-      priority: "very-high",
-      status: "Aberto"
-    });
-  });
 });
-
 // ✅ ADICIONAR YAPOKER - TODOS OS DIAS (segunda a sábado)
 dates.forEach(date => {
   const dayOfWeek = date.getDay();
